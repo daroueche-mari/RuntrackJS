c20545f80fd9d40dc920f136ab2d8328551f73d6
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-04-20 11:30:09 +0200

    Job04 fait

---
+++ jour01/Job04/script.js
@@ -0,0 +1,13 @@
+alert("Hello Javascript")
+
+function bisextile(année) {
+    if (année % 4 == 0)
+        return true
+    else {
+        return false
+    }
+}
+
+console.log(bisextile(2024))
+console.log(bisextile(2054))
+
