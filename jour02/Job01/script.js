0c7ca32593f8a51ac1efff53d926c9acf9cdc689
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-04-21 10:14:33 +0200

    Jour02 Job01 fait

---
+++ jour02/Job01/script.js
@@ -0,0 +1,7 @@
+function citation(){
+    let show = document.getElementById("citation")
+    myshow = show.innerHTML
+    console.log(myshow)
+}
+let btnaction = document.getElementById("button")
+btnaction.addEventListener("click", citation)
